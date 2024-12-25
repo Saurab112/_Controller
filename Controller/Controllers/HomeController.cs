d51384af97be5ef101f1b5da4b27e9e2daf791ab
Saurab Rajbanshi <saurab57322@gmail.com>
2024-12-25 09:24:55 +0545

    Initial commit

---
+++ Controller/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _Controller.Controllers
+{
+	//controller class inherits from controller
+	public class HomeController : Controller
+	{
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Submit(string name)
+		{
+			return RedirectToAction("Index");
+		}
+	}
+}
